2f2ccc66b80a933c135f6467ca891f407871247f
Colin2222 <crlew@outlook.com>
2024-03-26 18:22:45 -0400

    Improved integration states skeleton

--- IntegrationClasses.py
+++ IntegrationClasses.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+# MAJOR QUESTION: How does the BME handle their loop of controlling the motors?
+# Do we need to make our own timed frame-by-frame loop?
 
 class AbstractState(ABC):
     @abstractmethod
@@ -7,85 +9,58 @@ class AbstractState(ABC):
         pass
 
     @abstractmethod
-    def EvaluateAnatomy(self):
+    def Execute(self):
         pass
 
-    @abstractmethod
-    def AimAtAnatomy(self):
-        pass
 
-    @abstractmethod
-    def MoveDeeper(self):
-        pass
+class EvaluatingAnatomyState(AbstractState):
+    def ManualOverride(self):
+        return ManualState()
 
-    @abstractmethod
-    def MoveBackwards(self):
-        pass
+    def Execute(self):
+        # LOGIC HERE WILL EVALUATE CAMERA FEED AND RETURN AN AIMING STATE
+        return AimingState()
 
 
 class AimingState(AbstractState):
+    currentMotorX = 0
+    currentMotorY = 0
+    goalMotorX = 0
+    goalMotorY = 0
+
     def ManualOverride(self):
-        pass
+        return ManualState()
 
-    def EvaluateAnatomy(self):
-        pass
-
-    def AimAtAnatomy(self):
-        pass
-
-    def MoveDeeper(self):
-        pass
-
-    def MoveBackwards(self):
-        pass
+    def Execute(self):
+        # LOGIC HERE WILL MANIPULATE MOTORS TO THE DESIRED ANGLE, THEN RETURN A MOVING STATE
+        # IF VALID FURTHER PATH IS FOUND
+        return MovingState()
 
 
 class MovingState(AbstractState):
     def ManualOverride(self):
-        pass
+        return ManualState()
 
-    def EvaluateAnatomy(self):
-        pass
-
-    def AimAtAnatomy(self):
-        pass
-
-    def MoveDeeper(self):
-        pass
-
-    def MoveBackwards(self):
-        pass
-
-
-class ManualState(AbstractState):
-    def ManualOverride(self):
-        pass
-
-    def EvaluateAnatomy(self):
-        pass
-
-    def AimAtAnatomy(self):
-        pass
-
-    def MoveDeeper(self):
-        pass
-
-    def MoveBackwards(self):
-        pass
+    def Execute(self):
+        # LOGIC HERE WILL MOVE THE LINEAR ACTUATOR FURTHER DOWN THE THROAT A SPECIFIED DISTANCE,
+        # THEN RETURN AND EvaluatingAnatomyState
+        return EvaluatingAnatomyState()
 
 
 class RetractingState(AbstractState):
     def ManualOverride(self):
-        pass
+        return ManualState()
 
-    def EvaluateAnatomy(self):
-        pass
+    def Execute(self):
+        # LOGIC HERE WILL MOVE THE LINEAR ACTUATOR OUT OF THE THROAT A SPECIFIED DISTANCE,
+        # THEN RETURN AND EvaluatingAnatomyState
+        return EvaluatingAnatomyState()
 
-    def SeekAnatomy(self):
-        pass
 
-    def MoveDeeper(self):
-        pass
+class ManualState(AbstractState):
+    def ManualOverride(self):
+        return self
 
-    def MoveBackwards(self):
-        pass
+    def Execute(self):
+        # LOGIC HERE WILL CONTROL THE LINEAR ACTUATOR AND MOTORS BASED OFF OF USER INPUT
+        return self
