90ab6853425270985ce52faf5db35d3700b5a450
Declan O’Brien <80363579+declanobrien03@users.noreply.github.com>
2024-09-17 22:41:08 -0400

    make train and test files

---
+++ test.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+class Tester:
+    @staticmethod
+    def test(model: YOLO, dataset: str) -> None:
+        pass
+    
+if __name__ == "__main__":
+    pass
---
+++ train.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+class Trainer:
+    @staticmethod
+    def train(model: YOLO, dataset: str) -> None:
+        pass
+    
+if __name__ == "__main__":
+    pass
